8b91d2ee252d4e78e5a29aa3bdd808225aa181e3
DStasSK <DStasSK@yandex.ru>
2022-07-24 18:37:02 +0300

    step

--- js/main.js
+++ js/main.js
@@ -6,15 +6,45 @@ let y = 15;
 
 
 // тестовая генерация поля
-for (let i = 0; i<x; i++){
-	fild[i] = [];
-	let div = document.createElement('div');
-	for (let j=0; j<y-i; j++){
-		fild[i][j] = 0;
-		let e = document.createElement('i');
-		e.innerHTML = `${i}-${j}`;
-		div.appendChild(e);
-	}
-	box.appendChild(div);
+// for (let i = 0; i<x; i++){
+// 	fild[i] = [];
+// 	let div = document.createElement('div');
+// 	for (let j=0; j<y-i; j++){
+// 		fild[i][j] = 0;
+// 		let e = document.createElement('i');
+// 		e.innerHTML = `${i}-${j}`;
+// 		div.appendChild(e);
+// 	}
+// 	box.appendChild(div);
+// }
+// console.table(fild);
+
+
+// стартовая генерация поля в документе
+const LSget = (a) => window.localStorage.getItem(a);
+const LSset = (a,b)=>window.localStorage.setItem(a,b);
+try {
+	if(x != LSget('rows_fild_x') || y != LSget('rows_fild_y')) throw new Error();
+	let fild2 = LSget('rows_fild');
+	box.innerHTML = fild2;
+	// console.log('try');
 }
-console.table(fild);
+catch (e) {
+	let fild2 = '';
+	for (let i = 0; i<x; i++){
+		fild2 += '<div>';
+		fild[i] = [];
+		for (let j=0; j<y; j++){
+			fild2 +=`<i>${i}-${j}</i>`;
+			fild[i][j] = 0;
+		}
+		fild2 += '</div>'
+	}
+
+	box.innerHTML = fild2;
+	LSset("rows_fild", fild2);
+	LSset("rows_fild_x", x.toString());
+	LSset("rows_fild_y", y.toString());
+	// console.log('catch');
+}
+// window.localStorage.clear();

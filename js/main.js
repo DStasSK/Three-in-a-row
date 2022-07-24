856d3fdea06a64e1bd4ad85b93f702583f9222ac
DStasSK <DStasSK@yandex.ru>
2022-07-24 19:11:48 +0300

    step

--- js/main.js
+++ js/main.js
@@ -21,24 +21,43 @@ fild_box.innerHTML = fild;
 biom_box.innerHTML = fild;
 
 
+
 box = document.querySelector('.box');
-let boxColumns = document.querySelectorAll('.biom .col');
-// console.log(boxColumns);
+let biom_columns = document.querySelectorAll('.biom .col');
 console.log(box);
+// console.table(biom);
 console.log(biom_box);
 // console.log(biom_box.children);
-console.log(boxColumns[0].children);
-
+console.log(biom_columns[0].children);
 
 // biom[0][0] = 1;
 for (let i = 0; i<x; i++){
 	biom[i] = [];
-	// let biom_coll = biom_box.child[i];
 	for (let j=0; j<y; j++){
 		biom[i][j] = Math.round(Math.random()*6);
 		if(biom[i][j]){
-			boxColumns[i].children[j].classList.add(`el${biom[i][j]}`);
+			biom_columns[i].children[j].classList.add(`el${biom[i][j]}`);
 		}
 	}
 }
 console.table(biom);
+
+
+biom_box.onclick = (e)=>{
+	for (let el of biom){
+		let exit = false;
+			// console.log(e.target);
+			for (let el of biom_columns){
+				console.log(e.target);
+				// if(e.target===el){
+					// el.classList.add('pof');
+					// console.log(el);
+					// console.log(1);
+				// }
+				e.target.style.background = 'red';
+				exit = true;
+				break;
+			}
+		if(exit) break;
+	}
+}

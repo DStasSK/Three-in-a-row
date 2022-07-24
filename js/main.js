f50ca6c6d193bc88358ef3ab3a4cff26a7a845ce
DStasSK <DStasSK@yandex.ru>
2022-07-24 19:00:34 +0300

    step

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ let y = 15;
 let fild = '';
 for (let i = 0; i<x; i++){
 	fild += '<div class="col">';
-	biom[i] = [];
+	// biom[i] = [];
 	for (let j=0; j<y; j++){
 		fild +=`<i>${i}-${j}</i>`;
-		biom[i][j] = 0;
+		// biom[i][j] = 0;
 	}
 	fild += '</div>'
 }
@@ -23,6 +23,22 @@ biom_box.innerHTML = fild;
 
 box = document.querySelector('.box');
 let boxColumns = document.querySelectorAll('.biom .col');
-console.log(boxColumns);
+// console.log(boxColumns);
 console.log(box);
+console.log(biom_box);
+// console.log(biom_box.children);
+console.log(boxColumns[0].children);
+
+
+// biom[0][0] = 1;
+for (let i = 0; i<x; i++){
+	biom[i] = [];
+	// let biom_coll = biom_box.child[i];
+	for (let j=0; j<y; j++){
+		biom[i][j] = Math.round(Math.random()*6);
+		if(biom[i][j]){
+			boxColumns[i].children[j].classList.add(`el${biom[i][j]}`);
+		}
+	}
+}
 console.table(biom);
